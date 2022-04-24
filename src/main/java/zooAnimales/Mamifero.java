5a0aee8bb3fad423d29646fc628f122801d853ba
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-04-24 17:24:07 -0500

    Create Mamifero.java

---
+++ src/main/java/zooAnimales/Mamifero.java
@@ -0,0 +1,58 @@
+package zooAnimales;
+import java.util.ArrayList;
+public class Mamifero extends Animal{
+  private static ArrayList<Mamifero> listado;
+  public static int leones;
+  public static int caballos;
+  private boolean pelaje;
+  private int patas;
+  public Mamifero(String n,int e,String h,String g ,boolean colorPiel,int venenoso){
+    this.setNombre(n);
+    this.setEdad(e);
+    this.setHabitat(h);
+    this.setGenero(g);
+    this.pelaje=colorPiel;
+    this.patas=venenoso;
+    listado.add(this);
+    Animal.setTotalAnimales(1+Animal.getTotalAnimales());
+  }
+  static{
+    listado=new ArrayList<Mamifero>();
+  }
+  public Mamifero(){
+    listado.add(this);
+    Animal.setTotalAnimales(1+Animal.getTotalAnimales());
+  }
+  public static ArrayList<Mamifero> getListado(){
+    return listado;
+  }
+  public boolean getPelaje(){
+    return pelaje;
+  }
+  public int getPatas(){
+    return patas;
+  }
+  public static void setListado(ArrayList<Mamifero> l){
+    listado=l;
+  }
+  public void setPelaje(boolean c){
+    pelaje=c;
+  }
+  public void setPatas(int v){
+    patas=v;
+  } 
+  public static int cantidadMamiferos(){
+    return Mamifero.listado.size();
+  }
+  public static Mamifero crearCaballo(String n,int e,String g ){
+    caballos+=1;
+    return new Mamifero(n,e,"pradera",g,true,4);
+  }
+  public static Mamifero crearLeon(String n,int e,String g){
+    leones+=1;
+    return new Mamifero(n,e,"selva",g,true,4);
+  } 
+  public boolean isPelaje(){
+    return pelaje;
+  }
+}
