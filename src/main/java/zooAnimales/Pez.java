a2f3a112207cb6baafae6e6f326f38c322345780
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-04-24 17:24:23 -0500

    Create Pez.java

---
+++ src/main/java/zooAnimales/Pez.java
@@ -0,0 +1,58 @@
+package zooAnimales;
+import java.util.ArrayList;
+public class Pez extends Animal{
+  private static ArrayList<Pez> listado;
+  public static int salmones;
+  public static int bacalaos;
+  private String colorEscamas;
+  private int cantidadAletas;
+  public Pez(String n,int e,String h,String g ,String colorPiel,int venenoso){
+    this.setNombre(n);
+    this.setEdad(e);
+    this.setHabitat(h);
+    this.setGenero(g);
+    this.colorEscamas=colorPiel;
+    this.cantidadAletas=venenoso;
+    listado.add(this);
+    Animal.setTotalAnimales(1+Animal.getTotalAnimales());
+  }
+  static{
+    listado=new ArrayList<Pez>();
+  }
+  public Pez(){
+    listado.add(this);
+    Animal.setTotalAnimales(1+Animal.getTotalAnimales());
+  }
+  public static ArrayList<Pez> getListado(){
+    return listado;
+  }
+  public String getColorEscamas(){
+    return colorEscamas;
+  }
+  public int getCantidadAletas(){
+    return cantidadAletas;
+  }
+  public static void setListado(ArrayList<Pez> l){
+    listado=l;
+  }
+  public void setColorEscamas(String c){
+    colorEscamas=c;
+  }
+  public void setCantidadAletas(int v){
+    cantidadAletas=v;
+  } 
+  public static int cantidadPeces(){
+    return Pez.listado.size();
+  }
+  public static Pez crearSalmon(String n,int e,String g ){
+    salmones+=1;
+    return new Pez(n,e,"oceano",g,"rojo",6);
+  }
+  public static Pez crearBacalao(String n,int e,String g){
+    bacalaos+=1;
+    return new Pez(n,e,"oceano",g,"gris",6);
+  } 
+  public String movimiento(){
+    return "nadar";
+  }
+}
