788b937af778d7cfa681ccca62a7eee8cd3d4d3f
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-04-24 17:23:50 -0500

    Create Ave.java

---
+++ src/main/java/zooAnimales/Ave.java
@@ -0,0 +1,50 @@
+package zooAnimales;
+import java.util.ArrayList;
+public class Ave extends Animal{
+  private static ArrayList<Ave> listado;
+  public static int halcones;
+  public static int aguilas;
+  private String colorPlumas;
+  public Ave(String n,int e,String h,String g,String colorPiel){
+    this.setNombre(n);
+    this.setEdad(e);
+    this.setHabitat(h);
+    this.setGenero(g);
+    this.colorPlumas=colorPiel;
+    listado.add(this);
+    Animal.setTotalAnimales(1+Animal.getTotalAnimales());
+  }
+  static{
+    listado=new ArrayList<Ave>();
+  }
+  public Ave(){
+    listado.add(this);
+    Animal.setTotalAnimales(1+Animal.getTotalAnimales());
+  }
+  public static ArrayList<Ave> getListado(){
+    return listado;
+  }
+  public String getColorPlumas(){
+    return colorPlumas;
+  }
+  public static void setListado(ArrayList<Ave> l){
+    listado=l;
+  }
+  public void setColorPlumas(String c){
+    colorPlumas=c;
+  }
+  public static int cantidadAves(){
+    return Ave.listado.size();
+  }
+  public static Ave crearHalcon(String n,int e,String g){
+    halcones+=1;
+    return new Ave(n,e,"montanas",g,"cafe glorioso");
+  }
+  public static Ave crearAguila(String n,int e,String g){
+    aguilas+=1;
+    return new Ave(n,e,"montanas",g,"blanco y amarillo");
+  } 
+  public String movimiento(){
+    return "volar";
+  }
+}
